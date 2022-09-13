180369708488a4d856e344f04a6e12898dbe0d2c
=Hemapriya17 <=hema.learny@gmail.com>
2022-09-13 15:25:17 +0530

    Intro

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -7,7 +7,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#intro">Home</a>
         </li>
-        {/* <li onClick={()=>setMenuOpen(false)}>
+        <li onClick={()=>setMenuOpen(false)}>
           <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
@@ -18,7 +18,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#contact">Contact</a>
-        </li> */}
+        </li>
       </ul>
     </div>
   );
